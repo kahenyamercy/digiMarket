c2b53942e3031f9620c5ebddb60afb60785cf101
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-31 22:15:18 +0300

    Protect routes using for authorization

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
+import { useSelector } from "react-redux";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 function App() {
+  const { userInfo } = useSelector((state) => state.user);
+
+  const AuthLayout = () => {
+    if (userInfo) {
+      return <Outlet />;
+    } else {
+      return <Navigate to={"/login"} replace />;
+    }
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route element={<AuthLayout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
       </Routes>
     </Router>
   );
