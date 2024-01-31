048c9e683b22959cdadc1c7847482822a6e08dfc
kahenyamercy <kahenyamercy5@gmail.com>
2024-02-01 00:13:00 +0300

    Define product model

--- flask_server/models/config/db.py
+++ flask_server/models/config/db.py
@@ -3,48 +3,46 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
-from models.user import User, Base
+from models.user import User
+from models.base import Base
+from models.product import Product,  Category, ProductCategory
 
-classes = {"User": User}
-
-
+classes = {"User":User, "Product":Product, "Category":Category, "ProductCategory":ProductCategory}
 class DBConnection:
     __engine = None
     __session = None
-
+    
     def __init__(self):
         MYSQL_USER = getenv("MYSQL_USER")
         MYSQL_PWD = getenv("MYSQL_PWD")
         MYSQL_HOST = getenv("MYSQL_HOST")
-        MYSQL_DB = getenv("MYSQL_DB")
-        self.__engine = create_engine(
-            "mysql+pymysql://{}:{}@{}/{}".format(MYSQL_USER, MYSQL_PWD, MYSQL_HOST, MYSQL_DB))
-
+        MYSQL_DB= getenv("MYSQL_DB")
+        self.__engine = create_engine("mysql+pymysql://{}:{}@{}/{}".format(MYSQL_USER, MYSQL_PWD, MYSQL_HOST, MYSQL_DB))
+        
+    
     def close(self):
         self.__session.remove()
-
+    
     def reload(self):
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
-
+    
     def all(self, cls=None):
         """query on the current database session"""
-        new_dict = []
+        new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
                 objs = self.__session.query(classes[clss]).all()
                 for obj in objs:
-                    new_dict.append(obj)
+                    key = obj.__class__.__name__ + '.' + str(obj.id)
+                    new_dict[key] = obj
         return (new_dict)
-
-    def get(self, cls=None, pk=None):
-        # Assuming YourModel is the SQLAlchemy model class representing your table
-        obj = self.__session.query(cls).filter_by(username=pk).first()
-        return obj
-
-    def save(self, obj):
-        """adds obj to the current db session and commits them"""
+    
+    def create(self, obj):
+        """adds obj to the current db session"""
         self.__session.add(obj)
+        
+    def save(self):
+        """save all objects in the current db session and commit them"""
         self.__session.commit()
--- flask_server/models/user.py
+++ flask_server/models/user.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from models.base import BaseModel
-
-Base = declarative_base()
+from models.base import BaseModel, Base
 
 
 class Address(BaseModel, Base):
@@ -14,8 +11,8 @@ class Address(BaseModel, Base):
     county = Column(String(30))
     town = Column(String(30))
     village = Column(String(30), nullable=True)
-
-
+    
+    
 class User(BaseModel, Base):
     __tablename__ = 'user'
 
@@ -26,25 +23,26 @@ class User(BaseModel, Base):
     username = Column(String(255))
     password = Column(String(255))
     address_id = Column(Integer, ForeignKey('address.id'))
-    # address = relationship('Address', uselist=False, back_populates='user')
-
-
+    # address = relationship('Address', uselist=False, back_populates='user') 
+    
 class Farmer(User):
     __tablename__ = 'farmer'
 
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    # user = relationship('User', back_populates='farmer')
-
-
+    #user = relationship('User', back_populates='farmer')
+    
 class Wholesaler(User):
     __tablename__ = 'wholesaler'
 
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    # user = relationship('User', back_populates='wholesaler')
-
-
+    #user = relationship('User', back_populates='wholesaler')
+    
 class Retailer(User):
     __tablename__ = 'retailer'
 
     user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    # user = relationship('User', back_populates='retailer')
+    #user = relationship('User', back_populates='retailer')
+
+
+
+       
