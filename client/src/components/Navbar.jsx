ad9bc1af583b7a700d01b7bb0063f756d577c71e
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-02-06 23:14:44 +0300

    Add mobile responsiveness for topheader

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,23 +15,23 @@ const Navbar = () => {
   };
   return (
     <nav className=''>
-      <section className='flex gap-10 grid grid-cols-8 px-4'>
-        <div className='col-span-2 flex items-center'>
-          <h1 className='my-auto font-bold text-4xl text-lime-700'>
+      <section className='flex gap-4 md:gap-10 grid grid-cols-1 md:grid-cols-8 px-4 relative'>
+        <div className='col-span-1 md:col-span-2 flex items-center'>
+          <h1 className='mt-1 md:my-auto font-bold text-3xl md:text-4xl text-lime-700'>
             digiMarket
           </h1>
         </div>
-        <div className='p-4 flex gap-1 col-span-4'>
+        <div className='mb-4 md:pb-0 md:p-4 flex gap-1 col-span-1 md:col-span-4'>
           <input
             type='text'
             className='w-4/5 border border-gray px-3 py-2 rounded-lg text-lg'
             placeholder='Search product by name...'
           />
-          <button className='w-1/5 bg-lime-700 px-4 py-2 text-white border border-white rounded-lg'>
+          <button className='w-1/5 bg-lime-700 px-4 py-2 text-white border border-white rounded-lg text-sm md:text-md'>
             Search
           </button>
         </div>
-        <div className='col-span-2 flex items-center justify-end gap-4'>
+        <div className='col-span-1 md:col-span-2 flex items-center justify-center md:justify-end gap-2 md:gap-4 absolute top-1 right-1'>
           <div className='flex bg-slate-100 p-2 rounded-full text-lime-700 cursor-pointer'>
             <PersonOutlineIcon />
           </div>
@@ -49,7 +49,7 @@ const Navbar = () => {
           </div>
         </div>
       </section>
-      <div className='flex h-12 justify-between s-center px-4 bg-lime-300 text-gray-700 py-2'>
+      <div className='flex h-12 justify-between px-4 bg-lime-300 text-gray-700 py-2'>
         <section className='flex items-center'>
           <ul className='flex gap-3'>
             <li className='text-gray-800 uppercase cursor-pointer my-auto border-b border-transparent hover:border-b hover:border-gray-700'>
