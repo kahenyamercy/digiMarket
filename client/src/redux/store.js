ec6fd685065f40e60ab27f2c864bdc6c5d8ee056
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-02-19 00:43:26 +0300

    Handle cart items state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,6 +3,7 @@ import userReducer from "./slices/userSlices";
 import categoryReducer  from "./slices/categorySlices";
 import productReducer from "./slices/productSlices";
 import orderReducer from "./slices/orderSlices";
+import cartReducer from "./slices/cartSlices";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     category: categoryReducer,
     product: productReducer,
     order: orderReducer,
+    cart: cartReducer,
   },
 });
