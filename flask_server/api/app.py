6b01d5a9bece39591c28ff3b066736395f47b627
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-02-22 22:11:15 +0300

    Update server url

--- flask_server/api/app.py
+++ flask_server/api/app.py
@@ -9,14 +9,6 @@ import os
 # Load environment variables from .env
 load_dotenv()
 
-
-# Print loaded environment variables for debugging
-print("MYSQL_USER:", os.getenv("MYSQL_USER"))
-print("MYSQL_PWD:", os.getenv("MYSQL_PWD"))
-print("MYSQL_HOST:", os.getenv("MYSQL_HOST"))
-print("MYSQL_DB:", os.getenv("MYSQL_DB"))
-
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(api_views)
